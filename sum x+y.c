fc488d7144431b374d38898ea2d57a1be8d12223
Tulsi Chouhan <122629748+tulsichouhan@users.noreply.github.com>
2023-01-13 21:57:40 +0530

    Add files via upload

---
+++ sum x+y.c
@@ -0,0 +1,11 @@
+//c program to perform addition of two number statically and dynamically.
+
+#include<stdio.h>
+  int main()
+  {
+  	int y=12 ;
+  	float x=11.5,sum;
+  	sum=x+y;
+  	printf("%f",sum);
+  	return 0;
+  }
